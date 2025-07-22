fc60177b749235020f3b0a556ca5eb393cd8ff9d
malikasal <msalokhiddinova@myseneca.ca>
2025-07-22 21:34:02 +0000

    changes

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// main.cpp
+#include <iostream>
+#include "Interface.h"
+#include "AdminInterface.h"
+#include "StudentInterface.h"
+#include "Department.h"
+#include "Course.h"
+
+using namespace std;
+
+Department* StoreDepartments = nullptr;
+int TotalDepartments = 0;
+const char* csvFile = "data/courses.csv";
+
+int main() {
+    // Interface logic handled by Person B
+    return 0;
+}
