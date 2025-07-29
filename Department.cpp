64351fe17020e7fc1a3bbb5738cca3f20e9f71f1
J0shua-afk <joshualima900@gmail.com>
2025-07-29 08:45:26 -0400

    Update Department.cpp

--- Department.cpp
+++ Department.cpp
@@ -1,1 +1,30 @@
+#include "Department.h"
+#include <iostream>
+using namespace std;
 
+Department::Department(std::string name) : departmentName(name), courses(nullptr), totalCourses(0) {}
+
+Department::~Department() {
+    delete[] courses;
+}
+
+void Department::addCourse(const Course& course) {
+    Course* newCourses = new Course[totalCourses + 1];
+    for (int i = 0; i < totalCourses; ++i) {
+        newCourses[i] = courses[i];
+    }
+    newCourses[totalCourses] = course;
+    delete[] courses;
+    courses = newCourses;
+    totalCourses++;
+}
+
+void Department::displayCourses() const {
+    for (int i = 0; i < totalCourses; ++i) {
+        cout << i + 1 << ". ";
+        courses[i].displayCourse();
+    }
+}
+
+std::string Department::getDepartmentName() const { return departmentName; }
+int Department::getTotalCourses() const { return totalCourses; }
