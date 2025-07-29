091359041cbe7a028e1966305e5fabc8fa7a6d76
J0shua-afk <joshualima900@gmail.com>
2025-07-29 08:45:59 -0400

    Update Department.h

--- Department.h
+++ Department.h
@@ -1,1 +1,21 @@
+#ifndef DEPARTMENT_H
+#define DEPARTMENT_H
+#include "Course.h"
+#include <string>
 
+class Department {
+private:
+    std::string departmentName;
+    Course* courses;
+    int totalCourses;
+
+public:
+    Department(std::string name = "");
+    ~Department();
+    void addCourse(const Course& course);
+    void displayCourses() const;
+    std::string getDepartmentName() const;
+    int getTotalCourses() const;
+};
+
+#endif
