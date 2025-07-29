3b1c8252540a2f22bf85a0287e4b1b06e757d687
J0shua-afk <joshualima900@gmail.com>
2025-07-29 08:50:45 -0400

    Update AdminInterface.h

--- AdminInterface.h
+++ AdminInterface.h
@@ -1,1 +1,20 @@
+#ifndef ADMININTERFACE_H
+#define ADMININTERFACE_H
 
+#include "Department.h"
+
+class AdminInterface {
+private:
+    Department* departments;
+    int totalDepartments;
+
+public:
+    AdminInterface();
+    ~AdminInterface();
+    void listDepartments() const;
+    void addDepartment();
+    void addCourseToDepartment();
+    void saveToCSV(const char* filePath);
+};
+
+#endif
