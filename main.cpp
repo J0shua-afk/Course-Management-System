7ef5661154f426926a25a5b40c5aac4474217cd3
J0shua-afk <joshualima900@gmail.com>
2025-07-29 09:00:49 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,6 @@
-// main.cpp
 #include <iostream>
-#include "Interface.h"
+#include <fstream>
 #include "AdminInterface.h"
 #include "StudentInterface.h"
-#include "Department.h"
-#include "Course.h"
 
 using namespace std;
-
-Department* StoreDepartments = nullptr;
-int TotalDepartments = 0;
-const char* csvFile = "data/courses.csv";
-
-int main() {
-    // Interface logic handled by Person B
-    return 0;
-}
