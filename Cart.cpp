0c351ef1bd1f955a4a4ee4469fadfa2bf20bccfe
J0shua-afk <joshualima900@gmail.com>
2025-08-04 15:32:48 +0000

    Cart class manages the course selected for purchase. It has private data members for dynamically storing courses (cartCourses), their schedules (cartSchedules), the total number of courses, and total cost.

--- Cart.cpp
+++ Cart.cpp
@@ -1,1 +1,13 @@
+#ifndef CART_H
+#define CART_H
+#include "Course.h"
 
+class Cart {
+private:
+    Course* cartCourses;
+    std::string* cartSchedules;
+    int totalCourses;
+    double totalCost;
+
+
+#endif
