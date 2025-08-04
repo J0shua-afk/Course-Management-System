3603c270c20d8cc045f79cd562703341f0bb14e3
J0shua-afk <joshualima900@gmail.com>
2025-08-04 15:38:09 +0000

    changes

--- Cart.h
+++ Cart.h
@@ -1,1 +1,21 @@
+#ifndef CART_H
+#define CART_H
+#include "Course.h"
 
+class Cart {
+private:
+    Course* cartCourses;
+    std::string* cartSchedules;
+    int totalCourses;
+    double totalCost;
+
+public:
+    Cart();
+    ~Cart();
+    void addCourseToCart(const Course& course, const std::string& schedule);
+    void displayCart() const;
+    void checkout();
+    bool isEmpty() const;
+};
+
+#endif
