3b3ceafd791aec2736b4c101cd46e7f9608d579b
J0shua-afk <joshualima900@gmail.com>
2025-08-04 18:05:40 +0000

    changes

--- StudentInterface.cpp
+++ StudentInterface.cpp
@@ -1,1 +1,34 @@
+#include "StudentInterface.h"
+#include <iostream>
+using namespace std;
+
+StudentInterface::StudentInterface(Department* depts, int totalDepts) 
+    : departments(depts), totalDepartments(totalDepts) {}
+
+void StudentInterface::mainMenu() {
+    int choice;
+
+    do {
+        cout << "\nStudent Main Menu" << endl;
+        cout << "1. Show Cart" << endl;
+        cout << "2. Browse Departments" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice [1, 2, 3]: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            shoppingCart.displayCart();
+            break;
+        case 2:
+            browseDepartments();
+            break;
+        case 3:
+            cout << "Exiting program. Goodbye!" << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please try again." << endl;
+        }
+    } while (choice != 3);
+}
 
